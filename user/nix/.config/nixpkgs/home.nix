4d5db9deb83bc8f64c2a58424b09230f5aa68f0c
Diego Augusto S. C <diego.augusto@protonmail.com>
2022-07-18 17:44:12 -0300

    Port more stuff to nix

--- user/nix/.config/nixpkgs/home.nix
+++ user/nix/.config/nixpkgs/home.nix
@@ -7,12 +7,23 @@
   home.packages = with pkgs; [
     stow
     exa
+    ripgrep
+    fd
     zoxide
     direnv
+    wl-clipboard
+    wl-clipboard-x11
     # zsh plugins etc
     nix-zsh-completions
     zsh-syntax-highlighting
     zsh-completions
+    # dev stuff
+    python3
+    rustup
+    clang-tools
+    gcc
+    luajit
+    gdb
     ];
   programs.home-manager.enable = true;
   home.stateVersion = "22.05";
@@ -39,6 +50,42 @@
   programs.neovim = {
     package = pkgs.neovim-nightly;
     enable = true;
+    extraPackages = with pkgs; [
+        sumneko-lua-language-server
+        rust-analyzer
+        pyright
+        gopls
+        # Tree sitter stuff
+        tree-sitter
+        tree-sitter-grammars.tree-sitter-bash
+        tree-sitter-grammars.tree-sitter-c
+        tree-sitter-grammars.tree-sitter-cmake
+        tree-sitter-grammars.tree-sitter-commonlisp
+        tree-sitter-grammars.tree-sitter-cpp
+        tree-sitter-grammars.tree-sitter-css
+        tree-sitter-grammars.tree-sitter-dart
+        tree-sitter-grammars.tree-sitter-dockerfile
+        tree-sitter-grammars.tree-sitter-glsl
+        tree-sitter-grammars.tree-sitter-go
+        tree-sitter-grammars.tree-sitter-gomod
+        tree-sitter-grammars.tree-sitter-html
+        tree-sitter-grammars.tree-sitter-java
+        tree-sitter-grammars.tree-sitter-javascript
+        tree-sitter-grammars.tree-sitter-json
+        tree-sitter-grammars.tree-sitter-json5
+        tree-sitter-grammars.tree-sitter-latex
+        tree-sitter-grammars.tree-sitter-lua
+        tree-sitter-grammars.tree-sitter-make
+        tree-sitter-grammars.tree-sitter-markdown
+        tree-sitter-grammars.tree-sitter-nix
+        tree-sitter-grammars.tree-sitter-php
+        tree-sitter-grammars.tree-sitter-python
+        tree-sitter-grammars.tree-sitter-ruby
+        tree-sitter-grammars.tree-sitter-rust
+        tree-sitter-grammars.tree-sitter-toml
+        tree-sitter-grammars.tree-sitter-vim
+        tree-sitter-grammars.tree-sitter-zig
+    ];
     extraConfig =
     ''
     packadd packer.nvim
@@ -52,5 +99,4 @@
     '';
     plugins = with pkgs.vimPlugins; [ packer-nvim ];
   };
-  #programs.neovim = (import ./my-neovim.nix { inherit config pkgs; lib=pkgs.lib; });  
 }
