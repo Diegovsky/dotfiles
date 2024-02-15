5e5d0a2045990520a2d5f5bc407d5defc16df392
Diego Augusto S. C <diego.augusto@protonmail.com>
2024-02-14 21:08:37 -0300

    Did the remote dev setup

--- zsh/.config/zsh/devutils.zsh
+++ zsh/.config/zsh/devutils.zsh
@@ -2,23 +2,42 @@ function net::send() {
     meta::arg "$1" FILES 'to send' || return 5
     meta::arg "$2" REMOTE 'to send the files (will use ssh)' || return 5
 
-    rsync -azvhe "ssh -p ${PORT:-6534}" $@
+    local files=(${@[1,-2]})
+    local remote="${@[-1]}"
+
+    echo "Sending $files to $remote"
+    rsync -azvhe "ssh -p ${PORT:-6534}" $files $remote
+    return $?
 }
 
-function net::setup-recv() {
-    meta::arg "$1" REMOTE 'to connect (will use ssh)' || return 5
-    tempdir
+RECV_DIR="$HOME/Downloads/ssh-recv"
 
-    ssh -p ${PORT:-6534} -t "$1" env RECV_DIR=$PWD zsh -l
+function net::setup-run() {
+    meta::arg "$1" FILE 'to run' || return 5
+
+    mkdir -p "$RECV_DIR"
+    cd "$RECV_DIR"
+
+    local arquive="$1.xz"
+
+    while :; do
+        echo "Waiting to receive file..."
+        meta::watchfile "$arquive"
+        echo "Got it"
+        rm -f "$1"
+        xz -d "$arquive"
+        chmod +x "$1"
+        "./$1"
+    done
 }
 
 function rust::net-build() {
-    local remote="${RECV_DIR:-$1}"
-    meta::arg "$remote" REMOTE 'to send the file (will use ssh)' || return 5
+    meta::arg "$1" HOST "to send the file (will use ssh and place file at '${RECV_DIR/$HOME/~}')" || return 5
+    local remote="$1"
 
     exe="${EXE:-target/debug/$(basename $PWD)}"
 
-    cargo build
+    cargo build || return 5
 
     if [[ ! -f "$exe" ]]; then
         meta::err "Executable '$exe' not found. You can put its path in the \$EXE variable instead."
@@ -26,13 +45,13 @@ function rust::net-build() {
     fi
 
     local exename="$(basename $exe)"
-    local tarexe="$exename.tar.xz"
+    local arquive="$exename.xz"
 
-    strip "$exe"
+    strip "$exe" || return 5
 
     echo "Compressing '$exe'..."
-    tar -I xz -vcf "$tarexe" "$exe"
+    xz -c "$exe" > "$arquive" || return 5
     echo "Done!"
 
-    net::send "$tarexe" "$remote"
+    net::send "$arquive" "$remote:$RECV_DIR" || return 5
 }
