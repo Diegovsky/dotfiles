7c137f7d006bdbe6f20d71a978a662f9ab6c6dd2
Diego Augusto S. C <diego.augusto@protonmail.com>
2024-12-27 14:48:52 -0300

    Update scripts and ignores

--- private_dot_config/private_fish/interactive.fish
+++ private_dot_config/private_fish/interactive.fish
@@ -23,4 +23,13 @@ end
 set -g TERM_FOCUSED no
 bind \e\[I 'set -g TERM_FOCUSED yes'
 bind \e\[O 'set -g TERM_FOCUSED no'
+if test -n "$WT_SESSION"
+    set -gx COLORTERM truecolor
+end
 
+# add metadata for windows terminal
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+        printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
